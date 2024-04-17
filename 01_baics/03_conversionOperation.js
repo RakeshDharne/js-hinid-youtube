bd2af6f6c9b4475f02a235798cfd4544a462d0cd
RAKESH  DHARNE <113465364+RakeshDharne@users.noreply.github.com>
2024-04-17 18:00:25 +0000

    conversion complete

--- 01_baics/03_conversionOperation.js
+++ 01_baics/03_conversionOperation.js
@@ -40,3 +40,52 @@ let someNumber= 33
 let stringsomeNumber= String( someNumber)
 console.log( typeof stringsomeNumber);
 
+////////////////**************Operations*********************/
+
+
+let value=3
+let negValue= -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power
+// console.log(2%3); //remainder
+
+
+let str1="hitesh"
+
+let str2="Rakesh"
+
+let str3= str1+str2
+
+// console.log(str3);
+
+// console.log("1"+2) /// pehle stirng hai to vo string me convert hoga
+
+// console.log(1+"2");// pehle number hai to number me convert hoga
+
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+
+// console.log(+true)// op 1
+// console.log(+""); //op 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter=100
+
+gameCounter++;
+console.log(gameCounter);
+
+// link to study 
+
+//https:tc39.es/ecma262/multipage/abstract-operations.
+// html#sec-type-conversion
+  
