ae78c9b2d8a7eadbedb7fe3a2a1d4ed62d9d096e
RAKESH  DHARNE <113465364+RakeshDharne@users.noreply.github.com>
2024-04-17 18:26:57 +0000

    comparisoion

---
+++ 01_baics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+
+//problem faced when two datatype  is not same
+
+
+console.log("2">1);// true automatically convert string into number 
+console.log("02">1);// true 
+//always check that the comparision is between the same data type
+
+console.log(null>0) //false
+console.log(null==0)///false
+console.log(null>=0)//true
+ 
+// teh reason is that an equality check == and comparisions > < >= <= works differently
+// thats why null >=0 is true and null >0 si false
+// the above must not use
+
+
+console.log(undefined==0);
+
+//=== strict check
+// it check values and data types also
+
+console.log("2"===2);
+console.log(2===2);
